bcd613399f612b5335c831f5092f04f4a5fc5ceb
the-macs <esahadistra@gmail.com>
2023-08-23 13:11:44 +0700

    Initial Commit

---
+++ vfm-admin/_content/users/users.php
@@ -0,0 +1,23 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'rudy',
+    'pass' => '$1$VvhDXRGg$rW1xribTufDFx/nQXy18I1',
+    'role' => 'superadmin',
+  ),
+  3 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$7xMKiv3J$uNHsyq9t.aheXNNoMmKtj/',
+    'role' => 'superadmin',
+  ),
+  4 => 
+  array (
+    'name' => 'user',
+    'pass' => '$1$tNu3ty0D$2uaxIVSPcLIEMtjCmdhYt0',
+    'role' => 'user',
+    'disabled' => false,
+  ),
+);
